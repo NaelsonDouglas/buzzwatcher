b2b14a024bfe1678c6d4da46b92627cffacd9f57
naelsondouglas <naelson17@gmail.com>
2020-09-29 23:49:36 -0300

    Added the search tab

--- src/frontend/src/header.js
+++ src/frontend/src/header.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import { Menu } from 'antd';
+import React, { useState } from "react";
 import 'antd/dist/antd.css';
-import { PageHeader, Button } from 'antd';
+import { PageHeader,Form, Button } from 'antd';
+import { Input, Checkbox } from 'antd';
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+
+const onFinish = (values) => {
+  console.log('Success:', values);
+};
+
+const onFinishFailed = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
+
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
 
 
 class Header extends React.Component {
+
         constructor(props) {
           super(props);
           this.state = { isToggleOn: true };
@@ -18,18 +42,46 @@ class Header extends React.Component {
         render() {
           return (
                 <div>
-                        <PageHeader
-                        ghost={false}
-                        title="Title"
-                        extra={[
-                                <Button key="3">Operation</Button>,
-                                <Button key="2">Operation</Button>,
-                                <Button key="1" type="primary">
-                                Primary
-                                </Button>,
-                        ]}
-                        >
-                        </PageHeader>
+                        <Form
+                              {...layout}
+                              name="basic"
+                              initialValues={{
+                                remember: true,
+                              }}
+                              onFinish={onFinish}
+                              onFinishFailed={onFinishFailed}
+                            >
+                              <Form.Item
+                                label="Query"
+                                name="query"
+                                rules={[
+                                  {
+                                    required: true,
+                                    message: 'You must specify a search item',
+                                  },
+                                ]}
+                              >
+                                <Input placeholder="Ex: Playstation" />
+                              </Form.Item>
+                              <Form.Item
+                                label="Amount"
+                                name="Amount"
+                                rules={[
+                                  {
+                                    required: true,
+                                    message: 'Please input the amout of tweets.',
+                                  },
+                                ]}
+                              >
+                                <Input type="number" />
+                              </Form.Item>
+
+                              <Form.Item {...tailLayout}>
+                                <Button type="primary" htmlType="submit">
+                                  Search
+                                </Button>
+                              </Form.Item>
+                            </Form>
                 </div>
           );
         }
